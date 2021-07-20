257840030500d129872424326661137b1ed4a843
domuserk <rgdesenhos321@gmail.com>
2021-07-20 19:04:09 -0300

    feat(usersRepository): adjust authenctication

--- modules/users/repositories/implementations/usersRepository.ts
+++ modules/users/repositories/implementations/usersRepository.ts
@@ -31,15 +31,26 @@ class UsersRepository {
     }
 
     async verifyUserAccountSign({ emailOrUsername, password }) {
+
+        let format = "@gmail.com"
+        let isEmail = 'email',
+        isUsername = 'username';
       
+        const result = emailOrUsername.includes(format) ? await this.createQuerySearchUser(emailOrUsername, isEmail) : await this.createQuerySearchUser(emailOrUsername, isUsername);
+        return result;
+    }
+    
+    async createQuerySearchUser(emailOrUsername, type) {
+
         const resp =  await this.repository
         .createQueryBuilder("user") 
-        .where(`user.email = '${emailOrUsername}' `,)
+        .where(`user.${type} = '${ emailOrUsername }' `,)
         .execute();
-        
-        return resp;
-    }
 
+        const result = type == 'email' ? resp : resp;
+
+        return result;
+    }
     // async findByEmail({ email }) {
     //     return await this.repository.findOne(email);
     // }
